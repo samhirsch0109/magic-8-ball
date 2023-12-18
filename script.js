1bedf06cb03ede65019785eca1475d245b021564
Samantha Hirsch <sammyhirsch0109@gmail.com>
2023-12-18 13:18:36 -0500

    finished tutorial

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function shakeMagic8Ball() {
         document.getElementById('response-text').innerText = 'Please ask a question'
         document.getElementById('response-text').classList = 'text-danger'
         document.getElementById('response-text').src = '#BAD/BROKENIMAGE' //CHANGE 
+        return
     }
 
     if(!question.trim()) {
@@ -11,4 +12,59 @@ function shakeMagic8Ball() {
         return
     }
 
-}
+    const randomNumber = Math.floor(Math.random() * 8);
+    let answer, image, color;
+
+    switch(randomNumber){
+        case 0:
+            answer ="yes";
+            image ="yes-image";
+            color = "text-success";
+            break;
+            case 1:
+                answer = "No";
+                image = "no-image.jpg" //change this!!;
+                color = "text-danger";
+                break;
+            case 2:
+                answer = "Ask again later";
+                image = "later-image.jpg" //change this!!;
+                color = "text-secondary";
+                break;
+            case 3:
+                answer = "Maybe";
+                image = "maybe-image.jpg";//change this!!;
+                color = "text-warning";
+                break;
+            case 4:
+                answer = "Cannot predict now";
+                image = "cannot-predict-image.jpg";//change this!!;
+                color = "text-muted";
+                break;
+            case 5:
+                answer = "Don't count on it";
+                image = "dont-count-on-it-image.jpg";//change this!!;
+                color = "text-danger";
+                break;
+            case 6:
+                answer = "Most likely";
+                image = "most-likely-image.jpg";//change this!!;
+                color = "text-success";
+                break;
+            case 7:
+                answer = "Outlook not so good";
+                image = "outlook-not-good-image.jpg";//change this!!;
+                color = "text-danger";
+                break;
+            default:
+                break;
+        }
+    
+    const responseText = document.getElementById('response-text');
+    responseText.innerText = answer;
+    responseText.classList = `display-4 ${color}`;
+    document.getElementById('response-image').src = image;
+    }
+
+
+
